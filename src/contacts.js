06891688088d6e8d436a9181214c57cac7552ca4
Taras <kachmar.taras@gmail.com>
2024-07-19 15:33:10 +0300

    update header

--- src/contacts.js
+++ src/contacts.js
@@ -1,2 +1,3 @@
+import './js/header';
 import './js/mobile-menu';
 import './js/phone-mask';
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './js/header';
 import './js/mobile-menu';
 import './js/modal';
 import './js/phone-mask';
